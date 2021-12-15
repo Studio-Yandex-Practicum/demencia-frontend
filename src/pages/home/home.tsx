0dc79d115ef98e6f49be71c48f3045b54ac55ea0
Andrey Larikov <larikov174@gmail.com>
2021-12-16 00:06:13 +0500

    dev: add footer

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "./header";
+import Footer from "./footer";
 
 const HomePage: React.FC = () => {
   return (
     <div>
       <Header />
       <main></main>
-      <footer></footer>
+      <Footer />
     </div>
   );
 };

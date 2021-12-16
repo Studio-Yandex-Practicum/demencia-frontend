ef9fc3d9ad1b357526163efeca04f856f1ad3fc9
Andrey Larikov <larikov174@gmail.com>
2021-12-17 00:47:34 +0500

    feat: add slider(not workng!)

--- src/pages/home/main/hero/slider/slider.tsx
+++ src/pages/home/main/hero/slider/slider.tsx
@@ -1,10 +1,38 @@
 import React from "react";
 import image from "./img.webp";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import "./styles.css";
+
+const items = [
+  <div className="item" data-value="1">
+    <img src={image} width="100px" height="100px" />
+  </div>,
+  <div className="item" data-value="2">
+    <img src={image} width="100px" height="100px" />
+  </div>,
+  <div className="item" data-value="3">
+    <img src={image} width="100px" height="100px" />
+  </div>,
+  <div className="item" data-value="4">
+    <img src={image} width="100px" height="100px" />
+  </div>,
+  <div className="item" data-value="5">
+    <img src={image} width="100px" height="100px" />
+  </div>,
+];
 
 const Slider: React.FC = () => {
   return (
     <div>
-      <img src={image} width="100px" height="100px" />
+      <AliceCarousel
+        animationType="fadeout"
+        animationDuration={800}
+        disableButtonsControls
+        infinite
+        items={items}
+        mouseTracking
+      />
     </div>
   );
 };

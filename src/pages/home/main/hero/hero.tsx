c6c6f9dd35099b420b2a4509f7de62384b321de7
Andrey Larikov <larikov174@gmail.com>
2021-12-16 15:57:25 +0500

    feat: add slider component without slider itself

--- src/pages/home/main/hero/hero.tsx
+++ src/pages/home/main/hero/hero.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Test from "./test/test";
+import Slider from "./slider/slider";
 
 const Hero: React.FC = () => {
   return (
     <section>
       <Test />
+      <Slider />
     </section>
   );
 };
---
+++ src/pages/home/main/hero/slider/slider.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import image from "./img.webp";
+
+const Slider: React.FC = () => {
+  return (
+    <div>
+      <img src={image} width="100px" height="100px" />
+    </div>
+  );
+};
+
+export default Slider;

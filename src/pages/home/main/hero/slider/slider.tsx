f4fa33b9fcf64d704255f95e3686bd650905b135
Andrey Larikov <larikov174@gmail.com>
2021-12-17 11:48:38 +0500

    fix: add working swiper

--- src/pages/home/main/hero/slider/slider.tsx
+++ src/pages/home/main/hero/slider/slider.tsx
@@ -1,10 +1,43 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react/swiper-react";
+import SwiperCore, { Autoplay, Pagination, Navigation } from "swiper";
 import image from "./img.webp";
+import "./styles.css";
+import "swiper/swiper.min.css";
+import "swiper/modules/pagination/pagination.min.css";
+import "swiper/modules/navigation/navigation.min.css";
+
+SwiperCore.use([Autoplay, Pagination, Navigation]);
 
 const Slider: React.FC = () => {
   return (
     <div>
-      <img src={image} width="100px" height="100px" />
+      <Swiper
+        spaceBetween={10}
+        centeredSlides={true}
+        autoplay={{
+          delay: 4500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        className="myStyles"
+      >
+        <SwiperSlide>
+          slide 1
+          <img src={image} className="myImage" />
+        </SwiperSlide>
+        <SwiperSlide>
+          slide 2
+          <img src={image} className="myImage" />
+        </SwiperSlide>
+        <SwiperSlide>
+          slide 3
+          <img src={image} className="myImage" />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };

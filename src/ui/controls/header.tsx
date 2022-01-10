b748ef57d8cba37abeefcc38f50fff152a2815ad
Andrey Kruglov <kruglov.it@gmail.com>
2022-01-10 11:50:26 +0300

    fix unused imports

--- src/ui/controls/header.tsx
+++ src/ui/controls/header.tsx
@@ -1,13 +1,3 @@
-import styled, { ThemeProps, DefaultTheme } from "styled-components";
-import {
-  ButtonShape,
-  ButtonType,
-  PaletteColor,
-  ScreenSize,
-  TypographyLevel,
-} from "../types";
-import { Button, ButtonProps } from "./button";
-
 const Header = styled.header`
   width: 100%;
   background: transparent;

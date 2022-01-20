c038e8280ffd50611448acacaee3e2ff99359def
Andrey Larikov <larikov174@gmail.com>
2022-01-20 08:59:28 +0500

    fix: depricate unused files, move img to images

--- src/pages/home/hero/hero.tsx
+++ src/pages/home/hero/hero.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Test from "./test/test";
-import Slider from "./slider/sliderV2";
+import Slider from "./slider/slider";
 import { TwoColumnGrid, Section } from "../../../ui/controls/layout";
 
 const Hero: React.FC = () => {
--- src/pages/home/hero/slider/slider.tsx
+++ src/pages/home/hero/slider/slider.tsx
@@ -3,46 +3,102 @@ import styled from "styled-components";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Autoplay, Pagination, Navigation } from "swiper";
 // eslint-disable-next-line import/extensions
-import image from "./img.webp";
-import pinkHalfCircle from "../../../../images/decor_halfcirlce_pink.svg";
+import image from "../../../../images/pechkin.png";
+import cursorPointer from "../../../../images/cursor_pointer.svg";
+import whiteHalfCircle from "../../../../images/test_halfcircle_white.svg";
 import "./styles.css";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
-import { Box, Container, Link } from "../../../../ui/controls";
+import { Link } from "react-router-dom";
+import { Container, Box } from "../../../../ui/controls";
 import { ContainerSize, TextColor } from "../../../../ui/types";
 import { BackgroundColor } from "../../../../ui/types/background-color.enum";
 import { Text1 } from "../../../../ui/controls/typography";
+import { ScreenSize } from "../../../../ui/types";
 
 SwiperCore.use([Autoplay, Pagination, Navigation]);
+
+const Wrapper = styled.div`
+  position: absolute;
+  bottom: calc(100% / 2 - 175px / 0.65);
+  z-index: 1;
+  @media (max-width: ${ScreenSize.XSmall}px) {
+    bottom: calc(100% / 2 - 175px / 0.8);
+  }
+  @media (max-width: ${ScreenSize.XSmall}px) {
+    bottom: 0;
+  }
+`;
 
 const Image = styled.img.attrs((props) => ({
   src: props.src || image,
 }))`
-  width: 100%;
-  height: auto;
+  max-width: 905px;
+  width: 55vw;
+  @media (max-width: ${ScreenSize.Medium}px) {
+    max-width: 100%;
+    width: 100%;
+  }
 `;
 
 const Decor = styled.div`
-  width: 115px;
-  height: 220px;
-  background-image: url(${pinkHalfCircle});
+  width: 135px;
+  height: 266px;
+  background-image: url(${whiteHalfCircle});
   background-size: cover;
   background-repeat: no-repeat;
   position: absolute;
-  bottom: -20px;
-  left: 370px;
+  bottom: calc(100% / 2 - 266px / 2);
+  left: 77%;
+
+  @media (max-width: ${ScreenSize.Large}px) {
+    display: none;
+  }
+  @media (max-width: ${ScreenSize.Medium}px) {
+    display: block;
+  }
+  @media (max-width: ${ScreenSize.Small}px) {
+    display: none;
+  }
 `;
-const Wrapper = styled.div`
-  position: relative;
-  bottom: 210px;
+
+const LINK = styled.div`
+  a {
+    font-size: 20px;
+    line-height: 10px;
+    font-weight: 400;
+    color: white;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: border-bottom 0.5s cubic-bezier(0.2, -2, 0.8, 2);
+    cursor: url(${cursorPointer}), pointer;
+    &:hover {
+      border-bottom: 2px solid #6d097a;
+    }
+    @media (max-width: ${ScreenSize.Small}px) {
+      font-size: 16px;
+    }
+  }
+`;
+
+const StyledText = styled(Text1)`
+  @media (max-width: ${ScreenSize.Medium}px) {
+    font-size: 16px;
+  }
+`;
+
+const StyledBox = styled(Box)`
+  @media (max-width: ${ScreenSize.XSmall}px) {
+    margin: 8px;
+  }
 `;
 
 const Slider: React.FC = () => {
   return (
     <Swiper
-      spaceBetween={10}
+      spaceBetween={0}
       centeredSlides={true}
       autoplay={{
         delay: 1500,
@@ -51,43 +107,75 @@ const Slider: React.FC = () => {
       pagination={{
         clickable: true,
       }}
-      navigation={true}
+      navigation={{
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      }}
       className="myStyles"
     >
       <SwiperSlide>
-        <Image src={image} />
         <Wrapper>
-          <Decor />
-          <Container size={ContainerSize.Small} bgColor={BackgroundColor.Alt2}>
-            <Text1 mt={2} mb={1} ml={2} textColor={TextColor.Secondary}>
-              Фонд «Память поколений» и «Cоюзмультфильм» выпустили мультфильм о
-              диагностике когнитивных изменений
-            </Text1>
-            <Box ml={2} mb={1}>
-              <Link to="#" textColor={TextColor.Secondary}>
-                Подробнее...
-              </Link>
-            </Box>
+          <Container
+            size={ContainerSize.MediumSmall}
+            bgColor={BackgroundColor.Alt2}
+          >
+            <StyledBox mt={4} mb={2} ml={2} mr={12}>
+              <StyledText mb={2} textColor={TextColor.Secondary}>
+                ФОНД «ПАМЯТЬ ПОКОЛЕНИЙ» И «СОЮЗМУЛЬТФИЛЬМ» ВЫПУСТИЛИ МУЛЬТФИЛЬМ
+                О ДИАГНОСТИКЕ КОГНИТИВНЫХ ИЗМЕНЕНИЙ
+              </StyledText>
+              <LINK>
+                <Link to="/">Подробнее...</Link>
+              </LINK>
+            </StyledBox>
+            <Decor />
           </Container>
         </Wrapper>
+        <Image src={image} />
       </SwiperSlide>
       <SwiperSlide>
-        <Image src={image} />
         <Wrapper>
-          <Decor />
-          <Container size={ContainerSize.Small} bgColor={BackgroundColor.Alt2}>
-            <Text1 mt={2} mb={1} ml={2} textColor={TextColor.Secondary}>
-              Фонд «Память поколений» и «Cоюзмультфильм» выпустили мультфильм о
-              диагностике когнитивных изменений
-            </Text1>
-            <Box ml={2} mb={1}>
-              <Link to="#" textColor={TextColor.Secondary}>
-                Подробнее...
-              </Link>
-            </Box>
+          <Container
+            size={ContainerSize.MediumSmall}
+            bgColor={BackgroundColor.Alt2}
+          >
+            <StyledBox mt={4} mb={2} ml={2} mr={12}>
+              <StyledText mb={2} textColor={TextColor.Secondary}>
+                ФОНД «ПАМЯТЬ ПОКОЛЕНИЙ» И «СОЮЗМУЛЬТФИЛЬМ» ВЫПУСТИЛИ МУЛЬТФИЛЬМ
+                О ДИАГНОСТИКЕ КОГНИТИВНЫХ ИЗМЕНЕНИЙ
+              </StyledText>
+              <LINK>
+                <Link to="/">Подробнее...</Link>
+              </LINK>
+            </StyledBox>
+            <Decor />
           </Container>
         </Wrapper>
+        <Image src={image} />
       </SwiperSlide>
+      <SwiperSlide>
+        <Wrapper>
+          <Container
+            size={ContainerSize.MediumSmall}
+            bgColor={BackgroundColor.Alt2}
+          >
+            <StyledBox mt={4} mb={2} ml={2} mr={12}>
+              <StyledText mb={2} textColor={TextColor.Secondary}>
+                ФОНД «ПАМЯТЬ ПОКОЛЕНИЙ» И «СОЮЗМУЛЬТФИЛЬМ» ВЫПУСТИЛИ МУЛЬТФИЛЬМ
+                О ДИАГНОСТИКЕ КОГНИТИВНЫХ ИЗМЕНЕНИЙ
+              </StyledText>
+              <LINK>
+                <Link to="/">Подробнее...</Link>
+              </LINK>
+            </StyledBox>
+            <Decor />
+          </Container>
+        </Wrapper>
+        <Box />
+        <Image src={image} />
+      </SwiperSlide>
+      <div className="swiper-button-prev"></div>
+      <div className="swiper-button-next"></div>
     </Swiper>
   );
 };

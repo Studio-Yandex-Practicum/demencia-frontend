d286dc34e8ee31c324dd864e553f2bd908ac1e85
Ivan <bfg546@yandex.ru>
2022-01-27 15:20:38 +0300

    home/info-section. HTML code 100%

--- src/pages/home/info-section/info-section.tsx
+++ src/pages/home/info-section/info-section.tsx
@@ -3,6 +3,7 @@ import {
   Box,
   TwoColumnGrid,
   Subtitle1,
+  Subtitle3,
   Text4,
   Button,
 } from "../../../ui/controls";
@@ -11,54 +12,216 @@ import styled from "styled-components";
 import iPic from "../../../images/i_icon.svg";
 import pinkSemicirclePic from "../../../images/decor_halfcirlce_pink.svg";
 import greenPuzzleTranslucentPic from "../../../images/green-puzzle-translucent.svg";
+import purpleHalfGreenPuzzzlePic from "../../../images/purple-and-green-puzzle.svg";
+import grandparentsPic from "../../../images/grandparents.jpg";
+import greenSemicirclePic from "../../../images/green-semicircle.svg";
 
-const RelativeBox = styled(Box)<{
-  alignItems?: string;
-}>`
-  box-sizing: border-box;
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  justify-content: top;
-  align-items: ${({ alignItems }) => alignItems};
-  padding-top: 85px;
-  padding-right: 40px;
-  padding-left: 40px;
-
-  @media (max-width: ${ScreenSize.Medium}px) {
-    align-items: center;
-    padding-top: 40px;
-    padding-right: 20px;
-    padding-left: 20px;
+const StyledInfoSection = styled(Section)`
+  .info__about {
+    align-items: flex-end;
+    padding: 85px 40px;
   }
-`;
 
-const TwoColumnGridInfo = styled(TwoColumnGrid)`
-  grid-row-gap: 0;
+  .about__tip {
+    align-items: flex-start;
+    padding: 85px 40px;
+  }
+
+  .tip__box {
+    left: -65px;
+    max-width: 520px;
+    min-height: 170px;
+    padding-right: 170px;
+    z-index: 5;
+  }
+
+  .tip__i-pic {
+    width: 25px;
+    position: absolute;
+    top: calc(50% - 45px);
+    left: 80%;
+  }
+
+  .tip__pink-semicircle-pic {
+    height: 240px;
+    position: absolute;
+    top: calc(50% - 120px);
+    left: 75%;
+  }
+
+  .info__more-button-box {
+    align-items: flex-end;
+    padding: 0 40px;
+  }
+
+  .more-button-box__column {
+    max-width: 520px;
+    align-items: flex-start;
+  }
+
+  .about__column {
+    max-width: 520px;
+  }
+
+  .info__help-grid {
+    grid-template-areas:
+      "info__help info__grandparents"
+      "info__help info__test-button";
+  }
+
+  .info__puzzle-image {
+    width: 80px;
+    position: absolute;
+    top: -100%;
+    left: 10%;
+  }
+
+  .info__help {
+    grid-area: info__help;
+    align-items: flex-end;
+    padding: 230px 40px 120px 40px;
+  }
+
+  .help__column {
+    max-width: 235px;
+  }
+
+  .help__title {
+    text-align: right;
+  }
+
+  .help__subtitle {
+    text-align: right;
+  }
+
+  .help__puzzle-image {
+    width: 520px;
+    position: absolute;
+    top: 10%;
+  }
+
+  .info__grandparents {
+    grid-area: info__grandparents;
+    padding: 0;
+  }
+
+  .grandparents__image {
+    width: 600px;
+    position: absolute;
+    top: -20px;
+    left: -20px;
+  }
+
+  .info__test-button {
+    grid-area: info__test-button;
+    padding-top: 390px;
+  }
+
+  .info__test-button-column {
+    max-width: 590px;
+    align-items: center;
+  }
+
+  .test-button__side-semicircle {
+    width: 60px;
+    position: absolute;
+    top: -32px;
+    left: 270px;
+  }
 
   @media (max-width: ${ScreenSize.Medium}px) {
     .info__about {
-      padding-bottom: 40px;
-      grid-area: A;
+      grid-area: info__about;
+      align-items: center;
+      padding: 40px 20px;
     }
-    .info__tip {
-      grid-area: B;
+
+    .about__column {
+      align-items: center;
+      max-width: 100%;
     }
-    .info__button {
-      grid-area: C;
+
+    .about__tip {
+      grid-area: about__tip;
+      align-items: center;
+      padding: 40px 20px;
     }
+
+    .info__more-button-box {
+      grid-area: info__more-button-box;
+      align-items: center;
+      padding: 40px;
+    }
+
+    .more-button-box__column {
+      max-width: 100%;
+      align-items: center;
+    }
+
     .info__puzzle {
-      grid-area: D;
+      grid-area: info__puzzle;
     }
-    grid-template-areas:
-      "A"
-      "B"
-      "D"
-      "C";
+
+    .info__puzzle-image {
+      top: -35px;
+      left: 10%;
+    }
+
+    .info__about-grid {
+      grid-template-areas:
+        "info__about"
+        "about__tip"
+        "info__puzzle"
+        "info__more-button-box";
+    }
 
     .tip__box {
       left: 0;
       padding-right: 140px;
+    }
+
+    .info__help-grid {
+      grid-template-areas:
+        "info__grandparents"
+        "info__help"
+        "info__test-button";
+    }
+
+    .grandparents__image {
+      width: 100%;
+      position: static;
+    }
+
+    .info__help {
+      grid-area: info__help;
+      align-items: center;
+      padding: 20px;
+    }
+
+    .help__column {
+      max-width: 100%;
+      align-items: center;
+    }
+
+    .help__title {
+      text-align: center;
+    }
+
+    .help__subtitle {
+      text-align: center;
+    }
+
+    .help__puzzle-image {
+      display: none;
+    }
+
+    .info__test-button {
+      grid-area: info__test-button;
+      padding-top: 20px;
+    }
+
+    .info__test-button-column {
+      max-width: 100%;
     }
   }
 
@@ -73,57 +236,52 @@ const TwoColumnGridInfo = styled(TwoColumnGrid)`
       left: 78%;
       width: 58px;
     }
+
+    .info__test-button {
+      grid-area: info__test-button;
+      transform: scale(0.7);
+    }
   }
 `;
 
-const AbsoluteBox = styled(Box)`
-  display: flex;
+const RelativeBox = styled(Box)<{
+  alignItems?: string;
+}>`
+  box-sizing: border-box;
   position: relative;
-  left: -65px;
   display: flex;
-  max-width: 520px;
-  min-height: 170px;
-  padding-right: 170px;
-  z-index: 5;
+  flex-direction: column;
+  justify-content: top;
+  align-items: ${({ alignItems }) => alignItems};
 `;
 
-const FlexColumn = styled.div`
-  max-width: 520px;
+const TwoColumnGridInfo = styled(TwoColumnGrid)`
+  grid-row-gap: 0;
+`;
+
+const FlexColumn = styled.div<{
+  maxWidth?: string;
+  alignItems?: string;
+}>`
+  max-width: ${({ maxWidth }) => maxWidth};
   width: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: flex-start;
-
-  @media (max-width: ${ScreenSize.Medium}px) {
-    align-items: center;
-    max-width: 100%;
-  }
+  align-items: ${({ alignItems }) => alignItems};
 `;
 
-const StyledImg = styled.img<{
-  margin?: string;
-  position?: string;
-  top?: string;
-  left?: string;
-  width?: string;
-  height?: string;
-}>`
-  position: ${({ position }) => position};
-  top: ${({ top }) => top};
-  left: ${({ left }) => left};
+const StyledImg = styled.img`
   object-fit: contain;
   object-position: center;
-  width: ${({ width }) => width};
-  height: ${({ height }) => height};
 `;
 
 const InfoSection: React.FC = () => {
   return (
-    <Section borderBox flex centered>
-      <TwoColumnGridInfo width="100%">
-        <RelativeBox flex column alignItems="flex-end" className="info__about">
-          <FlexColumn>
+    <StyledInfoSection borderBox flex centered>
+      <TwoColumnGridInfo width="100%" className="info__about-grid">
+        <RelativeBox flex column className="info__about">
+          <FlexColumn className="about__column">
             <Subtitle1 mb={1}>О ДЕМЕНЦИИ</Subtitle1>
             <Text4>
               Деменция — это синдром, обычно хронический или прогрессирующий,
@@ -138,10 +296,9 @@ const InfoSection: React.FC = () => {
         </RelativeBox>
         <RelativeBox
           backgroundColor={PaletteColor.DarkPurple}
-          alignItems="flex-start"
-          className="info__tip"
+          className="about__tip"
         >
-          <AbsoluteBox
+          <RelativeBox
             backgroundColor={PaletteColor.LightGreen}
             className="tip__box"
           >
@@ -154,27 +311,20 @@ const InfoSection: React.FC = () => {
               появляется каждые 3 секунды. Деменция — это болезнь, а не
               нормальное проявление старения.
             </Text4>
-            <StyledImg
-              src={iPic}
-              width="25px"
-              position="absolute"
-              top="calc(50% - 45px)"
-              left="80%"
-              className="tip__i-pic"
-            />
+            <StyledImg src={iPic} className="tip__i-pic" />
             <StyledImg
               src={pinkSemicirclePic}
-              height="240px"
-              position="absolute"
-              top="calc(50% - 120px)"
-              left="75%"
               className="tip__pink-semicircle-pic"
             />
-          </AbsoluteBox>
+          </RelativeBox>
         </RelativeBox>
-        <RelativeBox alignItems="flex-end" className="info__button">
-          <FlexColumn>
-            <Button type={ButtonType.Secondary} ghost>
+        <RelativeBox className="info__more-button-box">
+          <FlexColumn className="more-button-box__column">
+            <Button
+              type={ButtonType.Secondary}
+              ghost
+              className="info__more-button"
+            >
               Подробнее
             </Button>
           </FlexColumn>
@@ -182,18 +332,44 @@ const InfoSection: React.FC = () => {
         <RelativeBox
           backgroundColor={PaletteColor.DarkPurple}
           className="info__puzzle"
-          zIndex={-1}
+          zIndex={1}
         >
           <StyledImg
             src={greenPuzzleTranslucentPic}
-            width="80px"
-            position="absolute"
-            top="30%"
-            left="10%"
+            className="info__puzzle-image"
           />
         </RelativeBox>
       </TwoColumnGridInfo>
-    </Section>
+
+      <TwoColumnGridInfo width="100%" className="info__help-grid">
+        <RelativeBox className="info__help">
+          <FlexColumn className="help__column">
+            <Subtitle1 className="help__title">ПОМОГИ БЛИЗКИМ</Subtitle1>
+            <Subtitle3 className="help__subtitle">
+              Пройди тест вместе с тем, кому нужна помощь
+            </Subtitle3>
+          </FlexColumn>
+          <StyledImg
+            src={purpleHalfGreenPuzzzlePic}
+            className="help__puzzle-image"
+          />
+        </RelativeBox>
+        <RelativeBox className="info__grandparents">
+          <StyledImg src={grandparentsPic} className="grandparents__image" />
+        </RelativeBox>
+        <RelativeBox className="info__test-button">
+          <FlexColumn className="info__test-button-column">
+            <Button type={ButtonType.Primary} width={300}>
+              Пройти тест
+              <StyledImg
+                src={greenSemicirclePic}
+                className="test-button__side-semicircle"
+              />
+            </Button>
+          </FlexColumn>
+        </RelativeBox>
+      </TwoColumnGridInfo>
+    </StyledInfoSection>
   );
 };
 
--- src/ui/controls/index.ts
+++ src/ui/controls/index.ts
@@ -18,7 +18,15 @@ import { LogoBlock } from "./logo";
 import { Overlay } from "./overlay";
 import { Sider } from "./sider";
 import Grid from "./grid";
-import { MainTitle, Subtitle1, Text2, Text3, Text4 } from "./typography";
+import {
+  MainTitle,
+  Title,
+  Subtitle1,
+  Subtitle3,
+  Text2,
+  Text3,
+  Text4,
+} from "./typography";
 
 export {
   Layout,
@@ -44,7 +52,9 @@ export {
   Sider,
   Grid,
   MainTitle,
+  Title,
   Subtitle1,
+  Subtitle3,
   Text2,
   Text3,
   Text4,

f286178b2a015f9af65aac244281ab4d253cd63a
Andrey Kruglov <kruglov.it@gmail.com>
2022-02-17 11:34:40 +0300

    routing

---
+++ src/pages/test/pages/question/question.tsx
@@ -0,0 +1,5 @@
+const QuestionPage = () => {
+  return <h1>Question</h1>;
+};
+
+export default QuestionPage;
---
+++ src/pages/test/pages/start/start.tsx
@@ -0,0 +1,5 @@
+const StartPage = () => {
+  return <h1>Start</h1>;
+};
+
+export default StartPage;

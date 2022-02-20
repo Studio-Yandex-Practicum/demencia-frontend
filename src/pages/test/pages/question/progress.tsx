fcc786211283d2739d9e007a70dfaade0a90707d
Andrey Larikov <larikov174@gmail.com>
2022-02-21 00:35:52 +0500

    feat: add set button status logic

--- src/pages/test/pages/question/progress.tsx
+++ src/pages/test/pages/question/progress.tsx
@@ -1,27 +1,42 @@
 import React from "react";
-import { useLocation, NavLink } from "react-router-dom";
+import styled from "styled-components";
 import { Box, Button } from "../../../../ui/controls";
 import { ButtonShape, ButtonType } from "../../../../ui/types";
 
+const StyledDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: min(1920px, 100%);
+  gap: 15px;
+  margin-top: 30px;
+`;
+
 interface Props {
-  pageID: number;
+  pageID: string;
 }
 
 const Progress: React.FC<Props> = ({ pageID }) => {
   const pagesArray = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
   ];
-  const location = useLocation().pathname;
-  console.log(location);
 
-  const renderDots = () =>
-    pagesArray.map((page) => (
-      <Box mb={2} key={page}>
-        <Button shape={ButtonShape.Circle} type={ButtonType.Alt}>
-          {page}
-        </Button>
-      </Box>
-    ));
+  const handleButtonStatus = (currentPage: number) => {
+    if (+pageID > currentPage) return ButtonType.Primary;
+    if (+pageID == currentPage) return ButtonType.Secondary;
+    return ButtonType.Alt;
+  };
+
+  const renderDots = () => (
+    <StyledDiv>
+      {pagesArray.map((page) => (
+        <Box mb={2} key={page}>
+          <Button shape={ButtonShape.Circle} type={handleButtonStatus(page)}>
+            {page}
+          </Button>
+        </Box>
+      ))}
+    </StyledDiv>
+  );
 
   return <div>{renderDots()}</div>;
 };
--- src/pages/test/pages/question/question.tsx
+++ src/pages/test/pages/question/question.tsx
@@ -3,6 +3,7 @@ import Progress from "./progress";
 
 const QuestionPage = () => {
   const path = useLocation().pathname;
+  const pageID = path.match(/\d/g);
 
   if (path === "/test/question") {
     return <Navigate to="/test/question/1" />;
@@ -14,7 +15,7 @@ const QuestionPage = () => {
       <Routes>
         <Route path=":id" element={<h2>Question</h2>} />
       </Routes>
-      <Progress pageID={1} />
+      <Progress pageID={pageID ? pageID[0] : ""} />
     </>
   );
 };

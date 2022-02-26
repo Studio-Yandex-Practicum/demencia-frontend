c51184246bf1e00d49882e10b7882450d9924702
Andrey Larikov <larikov174@gmail.com>
2022-02-26 18:13:55 +0500

    fix: reduce onHover zoom

--- src/pages/home/partners/slide.tsx
+++ src/pages/home/partners/slide.tsx
@@ -5,16 +5,11 @@ import { Link } from "../../../ui/controls";
 const StyledSlide = styled.div`
   background-color: #fff;
   box-sizing: border-box;
-  transition: all 1s ease;
   height: 100px;
-  &:hover {
-    transform: scale(1.1);
-  }
 `;
 
 const StyledImage = styled.img`
   object-fit: contain;
-  transition: all 1s ease;
   width: 100%;
   height: 100%;
 `;

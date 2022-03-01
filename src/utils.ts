afccac23f334ad1d2b8cb8bec261522617d99208
Andrey Larikov <larikov174@gmail.com>
2022-03-01 14:54:28 +0500

    fix: edit ellipsis logic

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 export default function textEllipsis(text: string, limit: number) {
-  if (text && limit) {
-    return text.length > limit && `${text.substring(0, limit)}...`;
+  if (text && limit && text.length > limit) {
+    return `${text.substring(0, limit)}...`;
   }
   return text;
 }

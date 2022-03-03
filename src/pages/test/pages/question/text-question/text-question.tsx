fb692e084d377c3372c75793a1755906347c15d4
unknown <elena.crivoscheina@yandex.ru>
2022-03-03 10:09:37 +0300

    add text-question and two-options-question

--- src/pages/test/pages/question/text-question/text-question.tsx
+++ src/pages/test/pages/question/text-question/text-question.tsx
@@ -1,21 +1,9 @@
 import styled from "styled-components";
-import { Box, Section, Subtitle1, Text1 } from "../../../../../ui/controls";
-import {
-  ScreenSize,
-  TextColor,
-  TypographyLevel,
-} from "../../../../../ui/types";
+import { useNavigate } from "react-router-dom";
+import { Box, Section } from "../../../../../ui/controls";
 import StyledInput from "../../../../../components/input-field";
 import { ArrowLeft, ArrowRight } from "./decor";
-import { testData } from "../../../data";
-
-const StyledSection = styled(Section)`
-  justify-content: flex-start;
-  flex-direction: row;
-  @media (max-width: ${ScreenSize.Small}px) {
-    flex-direction: column;
-  }
-`;
+import QuestionText from "../questionText";
 
 const StyledBoxInput = styled(Box)`
   flex-direction: row;
@@ -23,20 +11,6 @@ const StyledBoxInput = styled(Box)`
   align-items: center;
   margin: 90px auto;
   width: 100%;
-`;
-
-const StyleSubtitle1 = styled(Subtitle1)`
-  @media (max-width: ${ScreenSize.Small}px) {
-    margin: 0 auto;
-    font-size: 60px;
-  }
-`;
-
-const StyleText1 = styled(Text1)`
-  @media (max-width: ${ScreenSize.Small}px) {
-    margin: 20px auto;
-    font-size: 30px;
-  }
 `;
 
 const StyleArrowLeft = styled(ArrowLeft)`
@@ -48,33 +22,20 @@ const StyleArrowRight = styled(ArrowRight)`
 `;
 
 const TextQuestion: React.FC<{ number: number }> = ({ number }) => {
+  const navigate = useNavigate();
   return (
     <Box>
-      <StyledSection flex>
-        <StyleSubtitle1
-          textColor={TextColor.Shadow}
-          level={TypographyLevel.MainTitle}
-          maxWidth={90}
-        >
-          {number}
-        </StyleSubtitle1>
-        <StyleText1
-          maxWidth={810}
-          mt={6}
-          mr={5}
-          ml={0}
-          textColor={TextColor.Accent1}
-          level={TypographyLevel.Subtitle2}
-        >
-          {testData[number].question}
-        </StyleText1>
-      </StyledSection>
+      <QuestionText number={number} />
 
       <Section flex>
         <StyledBoxInput flex maxWidth={1900}>
-          <StyleArrowLeft />
+          <StyleArrowLeft
+            onClick={() => navigate(`/test/question/${number - 1}`)}
+          />
           <StyledInput />
-          <StyleArrowRight />
+          <StyleArrowRight
+            onClick={() => navigate(`/test/question/${number + 1}`)}
+          />
         </StyledBoxInput>
       </Section>
     </Box>
--- src/pages/test/pages/question/two-options-question/two-options-question.tsx
+++ src/pages/test/pages/question/two-options-question/two-options-question.tsx
@@ -1,5 +1,64 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Box, Section } from "../../../../../ui/controls";
+import { testData } from "../../../data";
+import QuestionText from "../questionText";
+import {
+  StyledBoxInput,
+  InputBox,
+  StyleBoxInputs,
+  StyleQuestionInputs,
+  StyleArrowLeft,
+  StyleArrowRight,
+  StyledInputList,
+  InputOne,
+  StyleLabel,
+} from "./two-options-question-styles";
+
 const TwoOptionsQuestion: React.FC<{ number: number }> = ({ number }) => {
-  return <h1>Question {number}</h1>;
+  const navigate = useNavigate();
+  const [checked, setChecked] = useState(true);
+
+  return (
+    <Box>
+      <QuestionText number={number} />
+      <Section flex>
+        <StyledBoxInput flex maxWidth={1900}>
+          <StyleArrowLeft
+            onClick={() => navigate(`/test/question/${number - 1}`)}
+          />
+          <StyleBoxInputs flex maxWidth={850}>
+            <StyleQuestionInputs>
+              <InputBox>
+                <StyledInputList
+                  name="a"
+                  type="radio"
+                  onChange={() => setChecked(!checked)}
+                />
+                <StyleLabel htmlFor="first">
+                  {testData[number].first}
+                </StyleLabel>
+              </InputBox>
+              <InputBox>
+                <StyledInputList
+                  name="a"
+                  type="radio"
+                  onChange={() => setChecked(checked)}
+                />
+                <StyleLabel htmlFor="second">
+                  {testData[number].second}
+                </StyleLabel>
+              </InputBox>
+            </StyleQuestionInputs>
+            {!checked && testData[number].needFirstDescription && <InputOne />}
+          </StyleBoxInputs>
+          <StyleArrowRight
+            onClick={() => navigate(`/test/question/${number + 1}`)}
+          />
+        </StyledBoxInput>
+      </Section>
+    </Box>
+  );
 };
 
 export default TwoOptionsQuestion;

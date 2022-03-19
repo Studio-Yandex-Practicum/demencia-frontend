67cebc259d2ebb1bb52e33061d93ecff7d34988c
AlexanderKuznetsov <alexanderanatolevic@yandex.ru>
2022-03-19 14:18:11 +0500

    style: align the test progress status buttons

--- src/pages/test/pages/question/components/progress.tsx
+++ src/pages/test/pages/question/components/progress.tsx
@@ -4,8 +4,9 @@ import { Box, Button } from "../../../../../ui/controls";
 import { ButtonShape, ButtonType } from "../../../../../ui/types";
 
 const StyledDiv = styled.div`
-  display: flex;
-  flex-wrap: wrap;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 50px);
+  justify-content: center;
   width: min(1920px, 100%);
   gap: 15px;
   margin-top: 30px;

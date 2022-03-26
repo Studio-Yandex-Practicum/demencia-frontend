834a3a2553c6aae4b7418a0d8487c1852175cf94
unknown <elena.crivoscheina@yandex.ru>
2022-03-26 21:13:27 +0300

    redact

--- src/pages/test/pages/question/clock-image-question/clock-image-question.tsx
+++ src/pages/test/pages/question/clock-image-question/clock-image-question.tsx
@@ -1,9 +1,11 @@
 import QuestionHeader from "../components/question-header";
+import QuestionTextUnfolding from "../components/question-text-unfolding";
 
 const ClockImageQuestion: React.FC<{ number: number }> = ({ number }) => {
   return (
     <>
       <QuestionHeader number={number} />
+      <QuestionTextUnfolding />
     </>
   );
 };

a2af4e9d3ce4b557befe1b241cc4bcb1dea19835
ritonpiton <rituhanych@yandex.ru>
2022-03-29 01:09:21 +0300

    feat: add textQuestion value to local storage, add styles

---
+++ src/pages/test/pages/question/text-question/text-question-styles.tsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import { Box, Text1 } from "../../../../../ui/controls";
+import { ScreenSize } from "../../../../../ui/types";
+
+export const StyledBoxArrowRight = styled(Box)`
+  margin-left: 10px;
+  @media (max-width: ${ScreenSize.MediumSmall}px) {
+    order: 2;
+    margin-left: 0;
+  }
+`;
+
+export const StyledBoxArrowLeft = styled(Box)`
+  margin-right: 10px;
+  @media (max-width: ${ScreenSize.MediumSmall}px) {
+    order: 1;
+    margin-right: 0;
+  }
+`;
+
+export const StyledBoxInput = styled(Box)`
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  margin: 90px auto 50px;
+  width: 100%;
+  @media (max-width: ${ScreenSize.MediumSmall}px) {
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    margin: 50px auto 30px;
+  }
+`;
+
+export const ErrorText = styled(Text1)`
+  text-align: center;
+  color: red;
+  margin: 20px auto;
+  font-size: 30px;
+  @media (max-width: ${ScreenSize.Small}px) {
+    font-size: 20px;
+  }
+`;
--- src/pages/test/pages/question/text-question/text-question.tsx
+++ src/pages/test/pages/question/text-question/text-question.tsx
@@ -1,20 +1,34 @@
-import styled from "styled-components";
-import { useNavigate } from "react-router-dom";
+import {
+  ErrorText,
+  StyledBoxArrowLeft,
+  StyledBoxArrowRight,
+  StyledBoxInput,
+} from "./text-question-styles";
 import { Box, Section } from "../../../../../ui/controls";
 import StyledInput from "../../../../../components/input-field";
 import { ArrowLeft, ArrowRight } from "../components/arrows";
 import QuestionHeader from "../components/question-header";
-
-const StyledBoxInput = styled(Box)`
-  flex-direction: row;
-  justify-content: space-around;
-  align-items: center;
-  margin: 90px auto;
-  width: 100%;
-`;
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const TextQuestion: React.FC<{ number: number }> = ({ number }) => {
   const navigate = useNavigate();
+
+  const [textAnswer, setTextAnswer] = useState("");
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.getItem(`${number}`)) {
+      const localAnswer = localStorage.getItem(`${number}`);
+      if (localAnswer) {
+        setTextAnswer(localAnswer);
+      }
+    }
+  }, [number]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTextAnswer(e.target.value);
+  };
 
   const onBack = () => {
     if (number > 1) {
@@ -23,20 +37,38 @@ const TextQuestion: React.FC<{ number: number }> = ({ number }) => {
   };
 
   const onForward = () => {
-    const to = number === 25 ? "/test/result" : `/test/question/${number + 1}`;
-    navigate(to);
+    if (textAnswer) {
+      setIsError(false);
+      localStorage.setItem(`${number}`, textAnswer);
+      const to =
+        number === 25 ? "/test/result" : `/test/question/${number + 1}`;
+      navigate(to);
+    } else setIsError(true);
   };
 
   return (
     <Box>
       <QuestionHeader number={number} />
 
-      <Section flex>
+      <Section centered flex>
         <StyledBoxInput flex maxWidth={1900}>
-          <ArrowLeft onClick={() => onBack()} />
-          <StyledInput />
-          <ArrowRight onClick={() => onForward()} />
+          <StyledBoxArrowLeft>
+            <ArrowLeft onClick={onBack} />
+          </StyledBoxArrowLeft>
+
+          <Box mb={4}>
+            <StyledInput onChange={handleChange} value={textAnswer} />
+          </Box>
+
+          <StyledBoxArrowRight>
+            <ArrowRight onClick={onForward} />
+          </StyledBoxArrowRight>
         </StyledBoxInput>
+        {isError && (
+          <ErrorText>
+            Необходимо ответить на вопрос, прежде, чем переходить к следующему
+          </ErrorText>
+        )}
       </Section>
     </Box>
   );

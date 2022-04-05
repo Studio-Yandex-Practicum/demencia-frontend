324a996d48371df5f304defb8142fcec6c13a058
Ivan <bfg546@yandex.ru>
2022-04-05 14:21:30 +0300

    Test. Test ID. Add auto navigate to last Ques-n

--- src/pages/test/test.tsx
+++ src/pages/test/test.tsx
@@ -1,8 +1,42 @@
-import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import {
+  Navigate,
+  Route,
+  Routes,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
 import { DescriptionPage, QuestionPage, ResultPage, StartPage } from "./pages";
 
 const TestPage = () => {
   const path = useLocation().pathname;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const lastQuestion = localStorage.getItem("last_question");
+    if (lastQuestion) {
+      if (isNaN(Number(lastQuestion))) {
+        switch (lastQuestion) {
+          case "result":
+            navigate("result");
+            break;
+          case "description":
+            navigate("description");
+            break;
+          case "start":
+            navigate("start");
+            break;
+          default:
+            navigate("start");
+            break;
+        }
+      } else {
+        navigate(`/test/question/${lastQuestion}`);
+      }
+    }
+    // console.log("1");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   if (path === "/test") {
     return <Navigate to="/test/start" />;

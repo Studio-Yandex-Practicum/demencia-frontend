9d534cd21cbece32c0df90cee77980447ba8eae6
Andrey Kruglov <kruglov.it@gmail.com>
2022-05-11 12:16:14 +0300

    fix

--- src/pages/test/pages/question/components/loading-text.ts
+++ src/pages/test/pages/question/components/loading-text.ts
@@ -1,21 +1,14 @@
 import styled from "styled-components";
-import { Text1 } from "../../../../../ui/controls";
+import { Subtitle3, Text1 } from "../../../../../ui/controls";
 import {
   ScreenSize,
   TextColor,
   TypographyLevel,
 } from "../../../../../ui/types";
 
-const LoadingText = styled(Text1).attrs(() => ({
-  level: TypographyLevel.Subtitle3,
-  textColor: TextColor.Accent1,
-}))`
+const LoadingText = styled(Subtitle3)`
   text-align: center;
   margin: 0 auto;
-  font-size: 30px;
-  @media (max-width: ${ScreenSize.Small}px) {
-    font-size: 20px;
-  }
 `;
 
 export default LoadingText;

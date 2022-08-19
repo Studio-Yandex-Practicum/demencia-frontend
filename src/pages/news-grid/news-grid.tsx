c653781add598e7e7eb1d4c6ba6b093f266246cd
Andrey Kruglov <kruglov.it@gmail.com>
2022-08-20 02:11:29 +0300

    fix news url

--- src/pages/news-grid/news-grid.tsx
+++ src/pages/news-grid/news-grid.tsx
@@ -133,7 +133,7 @@ const NewsGridPage: React.FC = () => {
               cardDateTime={article.createdAt}
               cardDateTimeText={formattedDate || "Дата новости"}
               imageSource={article.image || cardDefImg}
-              cardLinkTo={article.urlLabel || `/article/${article.id}`}
+              cardLinkTo={article.url || `/article/${article.id}`}
               urlLabel={article.urlLabel || "Подробнее"}
             />
           );

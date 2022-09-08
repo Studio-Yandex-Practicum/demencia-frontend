e4dd7ea69287894cb43ea5d6c31d70d32d493403
Alexander Dolzhenko <raskalovalexander@gmail.com>
2022-09-08 16:50:04 +0300

    refactor: the news block on the main page has been changed

--- src/pages/home/news/news.tsx
+++ src/pages/home/news/news.tsx
@@ -1,17 +1,31 @@
 import React from "react";
 import styled from "styled-components";
 import { Section } from "../../../ui/controls/layout";
-import { Container, Box } from "../../../ui/controls";
-import { PaletteColor, ScreenSize } from "../../../ui/types";
-import { Title, Text3 } from "../../../ui/controls/typography";
+import { Box, Button } from "../../../ui/controls";
+import {
+  ButtonType,
+  PaletteColor,
+  ScreenSize,
+  TextColor,
+  TypographyLevel,
+} from "../../../ui/types";
+import { Title, Text3, Text4 } from "../../../ui/controls/typography";
 import { Link } from "../../../ui/controls";
-import Slider from "./slider";
 import { useQuery } from "@apollo/client";
 import { SettingsData } from "../../../types/settings";
 import { GET_SETTINGS } from "../../../gql/query/settings";
 import AnimationWrapper from "../../../components/animation-wrapper";
+import { NewsArticlesData } from "../../../types/news";
+import { GET_NEWS_ARTICLES } from "../../../gql/query/news";
+import { useNavigate } from "react-router-dom";
 
-const StyledContainer = styled(Container)`
+const BoxVideoContainer = styled(Box)`
+  @media (max-width: ${ScreenSize.MediumSmall}px) {
+    flex-wrap: wrap;
+  }
+`;
+
+const BoxVideo = styled(Box)`
   align-items: flex-end;
   align-self: end;
   @media (max-width: ${ScreenSize.Small}px) {
@@ -20,118 +34,202 @@ const StyledContainer = styled(Container)`
   }
 `;
 
-const StyledSection = styled(Section)<{
-  minHeight?: string;
-  height?: string;
-}>`
-  min-height: ${(props) => props.minHeight};
-  height: ${(props) => props.height};
-  @media (max-width: ${ScreenSize.Large}px) {
-    height: 680px;
-  }
-  @media (max-width: ${ScreenSize.Medium}px) {
-    height: 645px;
-  }
-  @media (max-width: ${ScreenSize.Small}px) {
-    height: 575px;
-  }
-  @media (max-width: ${ScreenSize.XSmall}px) {
-    height: 525px;
-  }
-  @media (max-width: ${ScreenSize.XXSmall}px) {
-    height: 450px;
-  }
-`;
-
 const StyledBox = styled(Box)`
   @media (max-width: ${ScreenSize.Medium}px) {
-    padding-bottom: 48px;
+    flex-wrap: wrap;
   }
-  @media (max-width: ${ScreenSize.Small}px) {
-    padding: 16px 56px;
-    width: 100%;
-  }
-`;
-
-const StyledTitle = styled(Title)`
-  margin-left: 58px;
   transition: all 1s ease;
   ${({ animate }) =>
     animate ? "opacity: 0; transform: translateY(100px);" : ""}
+`;
 
+const CenteredBox = styled(Box)`
+  justify-content: center;
+`;
+
+const StyledTitle = styled(Title)`
+  transition: all 1s ease;
+  transition-duration: 0.7s;
+  transition-delay: 1;
+  ${({ animate }) =>
+    animate ? "opacity: 0; transform: translateX(-200px);" : ""}
   @media (max-width: ${ScreenSize.XSmall}px) {
     font-size: 30px;
   }
 `;
 
-const StyledWrapper = styled(Box)`
-  width: 100%;
-  text-align: end;
+const StyledText3 = styled(Text3)`
+  transition: all 1s ease;
+  ${({ animate }) =>
+    animate ? "opacity: 0; transform: translateX(-200px);" : ""}
 `;
 
-const StyledText3 = styled(Text3)`
-  font-size: 18px !important;
+const StyledImage = styled.img`
+  width: 100%;
+`;
+
+const CardContainer = styled(Box)`
+  max-width: 305px;
+
+  @media screen and (max-width: 1100px) {
+    max-width: 460px;
+  }
+  @media (max-width: ${ScreenSize.Medium}px) {
+    max-width: none;
+  }
 `;
 
 const DefaultCaption: React.FC<{ title: string }> = ({ title }) => <>{title}</>;
 
 const News: React.FC = () => {
+  const navigate = useNavigate();
   const { data } = useQuery<SettingsData>(GET_SETTINGS, {
     fetchPolicy: "cache-first",
   });
+
+  const onClick = () => {
+    navigate("/news-grid");
+  };
+
+  const arr = useQuery<NewsArticlesData>(GET_NEWS_ARTICLES);
+  const items = arr.data?.newsArticles.filter((el) => !!el);
 
   if (!data || !data.settings.newsSection)
     return <DefaultCaption title="Что нового?" />;
 
   if (!data || !data.settings.newsSectionUrlLabel)
-    return <DefaultCaption title="Перейти к ленте новостей" />;
+    return <DefaultCaption title="Лента новостей" />;
 
   const sectionTitle = data.settings.newsSection;
   const linkTitle = data.settings.newsSectionUrlLabel;
 
   if (!sectionTitle.length) return <DefaultCaption title="Что нового?" />;
-  if (!linkTitle.length)
-    return <DefaultCaption title="Перейти к ленте новостей" />;
+  if (!linkTitle.length) return <DefaultCaption title="Лента новостей" />;
 
   return (
-    <StyledSection id="news" flex centered height="735px">
-      <StyledSection
+    <Section flex centered backgroundColor={PaletteColor.LightGreen}>
+      <Box
+        mt={7}
+        mb={11}
+        pl={6}
+        pr={6}
         flex
-        centered
-        pt={3}
-        pr={3}
-        backgroundColor={PaletteColor.LightGreen}
-        borderBox
-        minHeight="90%"
+        column
+        maxWidth={1752}
+        width="100%"
       >
-        <StyledContainer>
-          <StyledWrapper>
+        <Box mb={7}>
+          <Box mb={4}>
             <AnimationWrapper>
               <StyledTitle ellipsis>{sectionTitle}</StyledTitle>
             </AnimationWrapper>
-          </StyledWrapper>
-          <Box mb={5} maxWidthPercent={90}>
-            <Link to="/news-grid">
-              <StyledText3 zoomOnHover ellipsis>
-                {linkTitle}
-              </StyledText3>
-            </Link>
           </Box>
-        </StyledContainer>
-      </StyledSection>
-      <StyledBox
-        width="calc(100% - 48px)"
-        pt={6}
-        pl={12}
-        pb={12}
-        pr={12}
-        backgroundColor={PaletteColor.DarkPurple}
-        absolute
-        bottom="0"
-      >
-        <Slider />
-      </StyledBox>
-    </StyledSection>
+          <AnimationWrapper>
+            <StyledText3 ellipsis>{linkTitle}</StyledText3>
+          </AnimationWrapper>
+        </Box>
+        <AnimationWrapper>
+          <StyledBox flex gap="20px" width="100%">
+            {items
+              ?.slice(0, 4)
+              .map(
+                ({
+                  id,
+                  image,
+                  title = "Заголовок новости",
+                  subTitle = "Скоро тут появится интересная новость",
+                  urlLabel = "Подробнее",
+                }) => {
+                  return (
+                    <Link
+                      key={id}
+                      zoomTextOnHover={false}
+                      borderBottomOnHover={false}
+                      to={`/article/${id}` || "/"}
+                      uppercase={false}
+                    >
+                      <CardContainer width="100%">
+                        <Box mb={2}>
+                          <StyledImage src={image} />
+                        </Box>
+                        <Box>
+                          <Box>
+                            <Text4>
+                              <strong>{title}</strong>
+                            </Text4>
+                          </Box>
+                          <Box mt={1}>
+                            <Text4>{subTitle}</Text4>
+                          </Box>
+                          <Link
+                            to={`/article/${id}` || "/"}
+                            uppercase={false}
+                            zoomTextOnHover={false}
+                            borderBottomOnHover={false}
+                          >
+                            <Box mt={2}>
+                              <Text4
+                                textColor={TextColor.Accent2}
+                                uppercase={false}
+                              >
+                                <strong>{urlLabel} &rarr;</strong>
+                              </Text4>
+                            </Box>
+                          </Link>
+                        </Box>
+                      </CardContainer>
+                    </Link>
+                  );
+                }
+              )}
+          </StyledBox>
+        </AnimationWrapper>
+
+        <CenteredBox flex mt={7} mb={18}>
+          <Button
+            type={ButtonType.Secondary}
+            level={TypographyLevel.Subtitle3}
+            onClick={onClick}
+          >
+            Все новости
+          </Button>
+        </CenteredBox>
+        <BoxVideoContainer flex gap="40px">
+          <BoxVideo>
+            <iframe
+              width="100%"
+              height="314px"
+              src="https://rutube.ru/play/embed/68eaf9ac4e89d3421a9c8cada1dfb3be"
+              frameBorder="0"
+            ></iframe>
+            <Box mt={2}>
+              <Text4>
+                <strong>
+                  Проект Деменция.net благотворительного фонда «Память
+                  поколений». Пройдите тест на сайте Деменция.net
+                </strong>
+              </Text4>
+            </Box>
+          </BoxVideo>
+          <BoxVideo>
+            <iframe
+              width="100%"
+              height="314px"
+              src="https://rutube.ru/play/embed/c69565afe0f59e85b55c6faeefd1aaa3"
+              frameBorder="0"
+            ></iframe>
+            <Box mt={2}>
+              <Text4>
+                <strong>
+                  Проект Деменция.net благотворительного фонда «Память
+                  поколений». Пройдите тест на сайте Деменция.net
+                </strong>
+              </Text4>
+            </Box>
+          </BoxVideo>
+        </BoxVideoContainer>
+      </Box>
+    </Section>
   );
 };
 
